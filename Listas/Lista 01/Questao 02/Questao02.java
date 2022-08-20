0580acf5379831cdbee084ae676581e2bb03bd5c
Joao Correia <joao.correia@vilt.local>
2022-08-20 15:25:52 -0300

    at2

--- Listas/Lista 01/Questao 02/Questao02.java
+++ Listas/Lista 01/Questao 02/Questao02.java
@@ -1,21 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class Questao02 {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         String origem;
         String destino;
+        String linha;
+        InputStream is;
+        InputStreamReader isr;
+        BufferedReader br;
+        BufferedWriter bw;
+        OutputStream os;
+        OutputStreamWriter osw;
+
         if (args.length < 2) {
             System.err.println("Sem argumentos");
             System.out.println("É necessário passar dois argumentos: origem e destino.");
         } else {
             origem = args[0];
             destino = args[1];
-            InputStream is = new FileInputStream(origem);
-            OutputStream os = new FileOutputStream(destino);
+            // Leitura do arquivo de origem.
+            is = new FileInputStream(origem);
+            isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
+            // Leitura do arquivo de destino.
+            os = new FileOutputStream(destino);
+            osw = new OutputStreamWriter(os);
+            bw = new BufferedWriter(osw);
+
+            while ((linha = br.readLine()) != null) {
+                bw.write(linha);
+                bw.newLine();
+            }
+
+            br.close();
+            bw.close();
         }
     }
 }
---
+++ Listas/Lista 01/Questao 02/Questao02maisSimples.java
@@ -0,0 +1,35 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Questao02maisSimples {
+    public static void main(String[] args) throws IOException {
+        String origem;
+        String destino;
+        InputStream is;
+        Scanner sc;
+        PrintStream ps;
+
+        if (args.length < 2) {
+            System.err.println("Sem argumentos");
+            System.out.println("É necessário passar dois argumentos: origem e destino.");
+        } else {
+            origem = args[0];
+            destino = args[1];
+            // Leitura do arquivo de origem.
+            is = new FileInputStream(origem);
+            sc = new Scanner(is);
+            // Leitura do arquivo de destino.
+            ps = new PrintStream(destino);
+
+            while (sc.hasNextLine()) {
+                ps.println(sc.nextLine());
+            }
+
+            sc.close();
+            ps.close();
+        }
+    }
+}
