58e93a1dc48799e43f11df417c6ac31b4f2f2987
Joao Correia <joao.correia@vilt.local>
2022-08-20 15:56:44 -0300

    at3

--- Listas/Lista 01/Questao 01/Questao01.java
+++ Listas/Lista 01/Questao 01/Questao01.java
@@ -1,67 +1,76 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Questao01 {
-    public static void main(String[] args) throws IOException {
-        String caminho;
+    public static void main(String[] args) {
         String linha;
         int tam = 3;
-        int n1 = 0;
+        int n1 = 1;
         int n2 = 0;
         String[] valores = new String[tam];
         InputStream is;
         InputStreamReader isr;
         BufferedReader br;
         Scanner sc = new Scanner(System.in);
+
         // leitura
         for (int i = 0; i < tam; i++) {
             valores[i] = sc.nextLine();
         }
+
         // Se o caminho não for definido.
         if (valores[0].equals("")) {
-            System.out.println("Cadê o caminho?");
+            System.out.println("Cadê o caminho ou nome do arquivo?");
             return;
         }
 
-        is = new FileInputStream(valores[0]);
-        isr = new InputStreamReader(is);
-        br = new BufferedReader(isr);
+        try {
+            is = new FileInputStream(valores[0]);
+            isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
 
-        if (!(valores[1].equals(""))) {
-            try {
-                n1 = Integer.parseInt(valores[1]);
-            } catch (NumberFormatException e) {
-                System.out.println(e);
-                System.out.println("O valor tem que ser inteiro!");
-                return;
+            if (!(valores[1].equals(""))) {
+                try {
+                    n1 = Integer.parseInt(valores[1]);
+                } catch (NumberFormatException e) {
+                    System.out.println(e);
+                    System.out.println("O valor tem que ser inteiro!");
+                    return;
+                }
             }
-        }
 
-        if (!(valores[2].equals(""))) {
-            try {
-                n2 = Integer.parseInt(valores[2]);
-            } catch (NumberFormatException e) {
-                System.out.println(e);
-                System.out.println("O valor tem que ser inteiro!");
-                return;
+            if (!(valores[2].equals(""))) {
+                try {
+                    n2 = Integer.parseInt(valores[2]);
+                } catch (NumberFormatException e) {
+                    System.out.println(e);
+                    System.out.println("O valor tem que ser inteiro!");
+                    return;
+                }
             }
-        }
 
-        for (int i = 0; (linha = br.readLine()) != null; i++) {
-            /*
-             * Se n2 == 0, significa que o n2 não foi definido, deve-se exibir as linhas do
-             * arquivo a partir de n1 até o final do arquivo.
-             * 
-             * Se n1 não for definido, deve-se exibir as linhas do arquivo a partir da
-             * primeira linha até n2.
-             */
-            if ((i >= n1) && (n2 == 0 || i <= n2))
-                System.out.println(linha);
+            for (int i = 1; (linha = br.readLine()) != null; i++) {
+                /*
+                 * Se n2 == 0, significa que o n2 não foi definido, deve-se exibir as linhas do
+                 * arquivo a partir de n1 até o final do arquivo.
+                 * 
+                 * Se n1 não for definido, deve-se exibir as linhas do arquivo a partir da
+                 * primeira linha até n2.
+                 */
+                if ((i >= n1) && (n2 == 0 || i <= n2))
+                    System.out.println(linha);
+            }
+            br.close();
+
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        br.close();
     }
 }
---
+++ Listas/Lista 01/Questao 01/Questao01maisSimples.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Questao01maisSimples {
+    public static void main(String[] args) throws IOException {
+        String caminho;
+        String linha;
+        String temp;
+        int n1 = 1;
+        int n2 = 0;
+        InputStream is;
+        InputStreamReader isr;
+        BufferedReader br;
+        Scanner sc = new Scanner(System.in);
+        // Leitura.
+        caminho = sc.nextLine();
+
+        // Se o caminho não for definido.
+        if (caminho.equals("")) {
+            System.out.println("Cadê o caminho?");
+            return;
+        }
+
+        temp = sc.nextLine();
+
+        if (!(temp.equals(""))) {
+            try {
+                n1 = Integer.parseInt(temp);
+            } catch (NumberFormatException e) {
+                System.out.println(e);
+                System.out.println("O valor tem que ser inteiro!");
+                return;
+            }
+        }
+
+        temp = sc.nextLine();
+
+        if (!(temp.equals(""))) {
+            try {
+                n2 = Integer.parseInt(temp);
+            } catch (NumberFormatException e) {
+                System.out.println(e);
+                System.out.println("O valor tem que ser inteiro!");
+                return;
+            }
+        }
+
+        // Leitura do arquivo.
+        is = new FileInputStream(caminho);
+        isr = new InputStreamReader(is);
+        br = new BufferedReader(isr);
+
+        int i = 1;
+        while ((linha = br.readLine()) != null) {
+            /*
+             * Se n2 == 0, significa que o n2 não foi definido, deve-se exibir as linhas do
+             * arquivo a partir de n1 até o final do arquivo.
+             * 
+             * Se n1 não for definido, deve-se exibir as linhas do arquivo a partir da
+             * primeira linha até n2.
+             */
+            if ((i >= n1) && (n2 == 0 || i <= n2))
+                System.out.println(linha);
+            i++;
+        }
+
+        br.close();
+    }
+}
