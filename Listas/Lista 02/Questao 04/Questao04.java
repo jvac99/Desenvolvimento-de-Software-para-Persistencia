2050d0aa8e54350dec17b99be2542a6469b94b75
jvac99 <jvac99@hotmail.com>
2022-08-30 17:32:52 -0300

    lista02at2

--- Listas/Lista 02/Questao 04/Questao04.java
+++ Listas/Lista 02/Questao 04/Questao04.java
@@ -2,20 +2,33 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Questao04 {
     public static void main(String[] args) {
         Properties prop = new Properties();
-        InputStream in;
+        InputStream is;
+        Scanner sc;
+        String nome;
+        int inicio;
+        int fim;
 
         try {
             // Leitura do arquivo de destino.
-            in = new FileInputStream("config.properties");
-            prop.load(in);
-            System.out.println(prop.getProperty("arquivo"));
-            System.out.println(prop.getProperty("linha_inicial"));
-            System.out.println(prop.getProperty("linha_final"));
-            in.close();
+            is = new FileInputStream("config.properties");
+            prop.load(is);
+            nome = prop.getProperty("arquivo");
+            inicio = Integer.parseInt(prop.getProperty("linha_inicial"));
+            fim = Integer.parseInt(prop.getProperty("linha_final"));
+            is.close();
+
+            is = new FileInputStream(nome);
+            sc = new Scanner(is);
+
+            for (int i = inicio; i <= fim && sc.hasNextLine(); i++) {
+                System.out.println(sc.nextLine());
+            }
+            is.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
